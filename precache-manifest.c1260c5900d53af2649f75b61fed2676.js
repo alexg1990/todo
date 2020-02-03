ee2eb6112f3fb3468b3684b812eb334066cbf601
Alex <alexg1990@users.noreply.github.com>
2020-02-03 22:09:34 +0100

    Updates

---
+++ precache-manifest.c1260c5900d53af2649f75b61fed2676.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2305014c2a0ed623b78ef317f3b6366a",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "122b8cbadc6b1ae5d008",
+    "url": "/todo/static/css/main.9fea3249.chunk.css"
+  },
+  {
+    "revision": "81fe39ab6413c311dbcf",
+    "url": "/todo/static/js/2.6e930ef4.chunk.js"
+  },
+  {
+    "revision": "122b8cbadc6b1ae5d008",
+    "url": "/todo/static/js/main.7f6990df.chunk.js"
+  },
+  {
+    "revision": "d1020702a58ef1497af5",
+    "url": "/todo/static/js/runtime-main.b12d2377.js"
+  }
+]);
